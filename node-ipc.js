45a0b8ee460c90a36c788a22cc5e5fbd7e65c26a
Theadd <pantallazo@gmail.com>
2014-07-27 20:40:52 +0200

    add ipc param in connect methods

--- node-ipc.js
+++ node-ipc.js
@@ -219,7 +219,7 @@ function connect(id,path,callback){
     
     ipc.of[id].connect();
     
-    callback();
+    callback(ipc);
 }
 
 function connectNet(id,host,port,callback){
@@ -289,7 +289,7 @@ function connectNet(id,host,port,callback){
     
     ipc.of[id].connect();
     
-    callback();
+    callback(ipc);
 }
 
 module.exports=ipc;
