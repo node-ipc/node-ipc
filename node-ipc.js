64fef8b3a52d4d38209fb55f2db2a801990ead4a
Brandon Nozaki Miller <brandon@diginow.it>
2021-07-07 19:59:31 +0000

    fixing docs

--- node-ipc.js
+++ node-ipc.js
@@ -3,16 +3,6 @@ import IPC from './services/IPC.js';
 class IPCModule extends IPC{
     constructor(){
         super();
-        //include IPC to make extensible
-        Object.defineProperty(
-            this,
-            'IPC',
-            {
-                enumerable:true,
-                writable:false,
-                value:IPC
-            }
-        )
     }
 }
 
